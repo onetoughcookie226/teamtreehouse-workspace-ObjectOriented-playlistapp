6be483873db923ae50ab40c3c6feeaca652e555f
onetoughcookie226 <onetoughcookie226@gmail.com>
2017-03-14 16:16:59 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by vagrant on 3/14/17.
+ */
+function Playlist() {
+
+}
+
+Playlist.prototype.add = function() {
+
+};
+
+Playlist.prototype.play = function() {
+
+};
+
+Playlist.prototype.stop = function(){
+
+};
+
+Playlist.prototype.next = function() {
+
+};
+
+Playlist.prototype.renderInElement = function() {
+
+};
